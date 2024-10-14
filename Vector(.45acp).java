8e1fe253b8434a4be0548a2cd08e592a580e5320
ceoEnsdrongo <150335445+ceoEsdrongo@users.noreply.github.com>
2024-10-14 09:50:24 +0200

    Vector(.45acp).java

--- Vector(.45acp).java
+++ Vector(.45acp).java
@@ -1,6 +1,7 @@
 ----------TestVector
   import java.util.Vector;
 
+
 public class TestVector {
     public static void main(String[] args) {
         Vector<Quadrato> v =new Vector<Quadrato>();
@@ -14,9 +15,22 @@ public class TestVector {
             Quadrato q = new Quadrato(i);
             v.add(q);
         }
+        /*System.out.println("Size = "+v.size());
+        System.out.println("Capacity = "+v.capacity());*/
+        Quadrato q1 = new Quadrato(11);
+        v.add(q1);
         System.out.println("Size = "+v.size());
         System.out.println("Capacity = "+v.capacity());
+        v.remove(0);
+        v.remove(1);
+        v.remove(2);
+        for (int i = 0; i < v.size(); i++) {
+            v.get(i).visualizza();
+        }
     }
+}
+
+
 }
 
 
