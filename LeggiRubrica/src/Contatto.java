d9209c40be72d36f97e2e11541f54a9c9d1263b6
ceoEnsdrongo <150335445+ceoEsdrongo@users.noreply.github.com>
2024-11-15 18:36:52 +0100

    Contatto.java

--- LeggiRubrica/src/Contatto.java
+++ LeggiRubrica/src/Contatto.java
@@ -1,25 +1,16 @@
-import java.util.Scanner;
-
 public class Contatto {
-
-    private String cognome, nome;
+    private String cognome;
+    private String nome;
     private double numero;
 
-    // Costruttore senza parametri
-    public Contatto() {
-        cognome = "";
-        nome = "";
-        numero = 0;
-    }
+    public Contatto() {}
 
-    // Costruttore con parametri
     public Contatto(String cognome, String nome, double numero) {
         this.cognome = cognome;
         this.nome = nome;
         this.numero = numero;
     }
 
-    // Getter e Setter
     public String getCognome() {
         return cognome;
     }
@@ -44,25 +35,8 @@ public class Contatto {
         this.numero = numero;
     }
 
-    // Metodo per inserire i dati del contatto
-    public void inserimento() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Inserisci il cognome:");
-        cognome = sc.nextLine();
-        System.out.println("Inserisci il nome:");
-        nome = sc.nextLine();
-        System.out.println("Inserisci il numero di telefono:");
-        numero = sc.nextDouble();
-        sc.nextLine();  // Pulisce il buffer
-    }
-
-    // Metodo per visualizzare il contatto
-    public void visualizza() {
-        System.out.println(this.toString());
-    }
-
     @Override
     public String toString() {
-        return "Contatto{" + "cognome='" + cognome + '\'' + ", nome='" + nome + '\'' + ", numero=" + numero + '}';
+        return "Cognome: " + cognome + ", Nome: " + nome + ", Numero: " + numero;
     }
 }
